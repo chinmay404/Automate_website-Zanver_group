68a1f038ee85071b52d4ad067f419af08a9fd288
chinmay404 <chinmaypisal45@gmail.com>
2023-07-02 13:32:11 +0530

    Working Selenium with One time init of driver

--- frontend/app1/apps.py
+++ frontend/app1/apps.py
@@ -1,6 +1,20 @@
+# app1/apps.py
 from django.apps import AppConfig
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+driver = None
 
 class App1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app1'
+    
+    def ready(self):
+        global driver
+        if driver is None:
+            chrome_options = Options()
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.executable_path = 'frontend/app1/chromedriver.exe'
+            driver = webdriver.Chrome(options=chrome_options)
+            print("Driver Started")
--- frontend/app1/views.py
+++ frontend/app1/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from app1.apps import driver
+from selenium.webdriver.common.by import By
 
-# Create your views here.
+def get_page(url):
+    print(f"[status] : Driver Getting {url}")
+    driver.get(url)
+    h1_element = driver.find_element(By.CSS_SELECTOR, 'h1.fs-3xl')
+    text = h1_element.text
+    return text
+
+def homepage(request):
+    try:
+        response = get_page('https://dev.to/mdrhmn/web-scraping-using-django-and-selenium-3ecg')
+        if response is not None:
+            return HttpResponse(f"Driver State : Working {response}")
+        else :
+            return HttpResponse(f"Driver State : NOT ABLE TO FETCH PAGE {response}")
+    except Exception as e:
+        return HttpResponse("Driver Error: " + str(e))
+
+def shutdown(request):
+    # No need to close the driver here as it will be handled by the MyAppConfig class
+    return HttpResponse("Server shutdown")
